f64f94811f6c92203af3d203754bfd0dddbf9077
chenyl <chenyl@datatowisdom.cn>
2019-04-09 11:05:46 +0800

    first commit

---
+++ strom/src/main/java/com/ylchen/strom/StromApplication.java
@@ -0,0 +1,13 @@
+package com.ylchen.strom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StromApplication.class, args);
+    }
+
+}
